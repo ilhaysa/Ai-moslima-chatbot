be32eb58161527f8b9484ef734e13456ae67ad71
ilhaysa <123863813+ilhaysa@users.noreply.github.com>
2025-06-29 02:14:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 Flask==2.3.2
 openai
-#!/bin/bash
 pip install -r requirements.txt
 python app.py
 #!/bin/bash
 pip install -r requirements.txt
 python app.py
+git add start.sh
+git commit -m "Fix: gebruik python3 in start.sh"
+git push
